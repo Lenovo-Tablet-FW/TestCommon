c45ab3d63704ddcf6b7938590d64483a2add9e3b
tornaco <tornaco@163.com>
2017-08-07 18:29:30 +0800

    Add Assets

---
+++ common/src/main/java/com/android/common/Assets.java
@@ -0,0 +1,48 @@
+package com.android.common;
+
+import android.content.res.AssetManager;
+import android.support.test.InstrumentationRegistry;
+
+import org.junit.Assert;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by guohao4 on 2017/8/7.
+ * Email: Tornaco@163.com
+ */
+public class Assets {
+
+    public static void copyFromAssetsTo(String from, String to) throws IOException {
+
+        if (!new File(to).getParentFile().exists()) {
+            Files.createParentDirsInShell(new File(to));
+        }
+
+        // 1. Copy to tmp. --> Java
+        // 2. Copy to dest.--> Shell
+        // 3. Remove tmp. -- Shell
+
+        File tmp = new File(InstrumentationRegistry
+                .getTargetContext()
+                .getCacheDir().getPath()
+                + File.separator + from);
+        Files.createParentDirsInShell(tmp);
+
+        Assert.assertTrue(com.google.common.io.Files.asByteSink(tmp).writeFrom(openInput(from)) > 0);
+        Files.copyInShell(tmp.getAbsolutePath(), to);
+
+        // Remove tmp.
+        Files.deleteInShell(tmp.getPath());
+    }
+
+    public static InputStream openInput(String fileName) throws IOException {
+        return openAsset().open(fileName);
+    }
+
+    public static AssetManager openAsset() {
+        return InstrumentationRegistry.getTargetContext().getAssets();
+    }
+}
