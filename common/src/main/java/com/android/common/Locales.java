1fc0e6860389301d002edf890b1a402bba8031ef
tornaco <tornaco@163.com>
2017-08-08 13:43:20 +0800

    Fix locale

--- common/src/main/java/com/android/common/Locales.java
+++ common/src/main/java/com/android/common/Locales.java
@@ -3,8 +3,6 @@ package com.android.common;
 import android.content.res.Configuration;
 import android.support.test.InstrumentationRegistry;
 
-import java.util.Locale;
-
 /**
  * Created by guohao4 on 2017/8/8.
  * Email: Tornaco@163.com
@@ -15,9 +13,6 @@ public class Locales {
     public static boolean isEn() {
         Configuration configuration = InstrumentationRegistry.getTargetContext()
                 .getResources().getConfiguration();
-        return configuration.locale == Locale.ENGLISH
-                || configuration.locale == Locale.US
-                || configuration.locale == Locale.UK
-                || configuration.locale == Locale.CANADA;
+        return configuration.locale.toString().contains("en");
     }
 }
