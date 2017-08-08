23986774a7c453443210d1198c39130089b562ae
tornaco <tornaco@163.com>
2017-08-08 12:18:35 +0800

    Bring up Locations

--- common/src/main/java/com/android/common/Locations.java
+++ common/src/main/java/com/android/common/Locations.java
@@ -1,5 +1,12 @@
 package com.android.common;
 
+import android.support.test.InstrumentationRegistry;
+import android.support.test.uiautomator.UiDevice;
+
+import junit.framework.Assert;
+
+import java.io.IOException;
+
 /**
  * Created by guohao4 on 2017/8/8.
  * Email: Tornaco@163.com
@@ -7,11 +14,12 @@ package com.android.common;
 
 public class Locations {
 
-    @Deprecated
-    public static void setEnabled(boolean enabled) {
+    public static void setGpsProviderEnabled(boolean enabled) throws IOException {
         String cmd =
                 enabled ?
-                        String.format("settings put secure location_providers_allowed %s", "network,gps")
-                        : String.format("settings put secure location_providers_allowed %s", "network,gps");
+                        String.format("settings put secure location_providers_allowed %s", "+gps")
+                        : String.format("settings put secure location_providers_allowed %s", "-gps");
+        String res = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation()).executeShellCommand(cmd);
+        Assert.assertTrue("Fail toggle gps", res == null || !res.contains("Err"));
     }
 }
