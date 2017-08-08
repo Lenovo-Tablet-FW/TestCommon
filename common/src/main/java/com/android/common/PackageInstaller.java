4cc38f928823c78138bf8e5c3454581101ab5d8b
tornaco <tornaco@163.com>
2017-08-08 09:56:34 +0800

    Add package installer

---
+++ common/src/main/java/com/android/common/PackageInstaller.java
@@ -0,0 +1,29 @@
+package com.android.common;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.uiautomator.UiDevice;
+
+import junit.framework.Assert;
+
+import org.newstand.logger.Logger;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by guohao4 on 2017/8/8.
+ * Email: Tornaco@163.com
+ */
+
+public class PackageInstaller {
+
+    public static void installFromAssets(String name) throws IOException {
+        String tmpPath = com.google.common.io.Files.createTempDir().getPath() + File.separator + "tmp.apk";
+        Assets.copyFromAssetsTo(name, tmpPath);
+        Logger.i("Installing with tmp apk: %s", tmpPath);
+        String cmd = String.format("pm install -r %s", tmpPath);
+        String res = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation())
+                .executeShellCommand(cmd);
+        Assert.assertTrue("Fail install package:" + name, res == null || !res.contains("Err"));
+    }
+}
